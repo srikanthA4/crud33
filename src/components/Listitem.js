66d6742dd8434f615cde4d1af6eb3a008b6d5a68
srikanthA4 <94035252+srikanthA4@users.noreply.github.com>
2022-03-29 22:23:35 +0530

    Update and rename Item.js to Listitem.js

---
+++ src/components/Listitem.js
@@ -0,0 +1,39 @@
+ import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import EditIcon from "@mui/icons-material/Edit";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+export default function Listitem({ user, count }) {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://623868a200ed1dbc5ab2754c.mockapi.io/users")
+      .then((response) => setUsers(response.data.reverse()));
+  }, [users]);
+
+  const deleteHandler = () => {
+    axios.delete(
+      `https://623868a200ed1dbc5ab2754c.mockapi.io/users/${user.id}`
+    );
+  };
+
+  return (
+    <tr className="text-center">
+      <td>{count}</td>
+      <td>{user.name}</td>
+      <td>{user.userId}</td>
+      <td>{user.accountType}</td>
+      <td>{user.city}</td>
+      <td>
+        <Link to={`/Edit/${user.id}`}>
+          <EditIcon />
+        </Link>
+        <IconButton onClick={deleteHandler} aria-label="delete">
+          <DeleteIcon />
+        </IconButton>
+      </td>
+    </tr>
+  );
+}
